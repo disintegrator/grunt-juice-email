2ff0c3ddf551ae7d0632df0223fee9ff3cd63687
George Haidar <george@pollen.com.au>
2013-08-16 17:56:35 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# grunt-juice-email
+
+> Inline stylesheets into email HTML templates using LearnBoost's Juice
+
+## Getting Started
+This plugin requires Grunt `~0.4.1`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-juice-email --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-juice-email');
+```
+
+## The "juice" task
+
+### Overview
+In your project's Gruntfile, add a section named `juice` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  juice: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      // Target-specific file lists and/or options go here.
+    },
+  },
+})
+```
+
+### Options
+
+This task's options are the same as those passed to the juice function.
+The available options can be found [here](https://github.com/LearnBoost/juice#juicefilepath-options-callback).
+
+### Usage Examples
+
+```js
+grunt.initConfig({
+  juice: {
+    options: {},
+    files: {
+      'dest/email-template.html': 'src/email-template.html',
+    },
+  },
+})
+```
+
---
+++ tasks/juice.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-juice-email
+ * https://github.com/disintegrator/grunt-juice-email
+ *
+ * Copyright (c) 2013 George Haidar
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+var juice = require('juice');
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('juice',
+      'Inline stylesheets into email HTML templates using LearnBoost\'s Juice',
+      function() {
+        var options = this.options({}),
+            done = this.async(),
+            jobs = [];
+
+        // Iterate over all specified file groups.
+        this.files.forEach(function(f) {
+          // Concat specified files.
+          var src = f.src.filter(function(filepath) {
+            // Warn on and remove invalid source files (if nonull was set).
+            if (!grunt.file.exists(filepath)) {
+              grunt.log.warn('Source file "' + filepath + '" not found.');
+              return false;
+            }
+            return true;
+          }).forEach(function(filepath) {
+            jobs.push(function(callback) {
+              juice(filepath, options, function(err, html) {
+                if (err) {
+                  callback(err);
+                  grunt.warn(err, 3);
+                }
+                else {
+                  grunt.file.write(f.dest, html);
+                  grunt.log.writeln('File "' + f.dest + '" created.');
+                  callback();
+                }
+              });
+            });
+          });
+        });
+
+        grunt.util.async.series(jobs, done);
+      });
+};
